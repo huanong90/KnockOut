f9bfb1435e44bc40a0faac7b41894f7d47c63fae
huanong90 <huanong90@163.com>
2017-04-04 14:15:06 +0800

    Text Binding - Value Binding - Computed Binding

---
+++ KnockOut Learning/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace KnockOut_Learning.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
